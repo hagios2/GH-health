7d32ec2c93f05f0798861c9f6ea38ff4989d9abb
hagios <hagioswilson@gmail.com>
2020-06-14 17:27:27 +0100

    commented campus seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CampusTableSeeder::class);
+        //$this->call(CampusTableSeeder::class);
+
+        $this->call(CategoriesTableSeeder::class);
     }
 }
