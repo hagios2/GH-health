160bb8970de98d43b913e5268275aa803193f2a9
hagios <hagioswilson@gmail.com>
2020-06-15 21:45:58 +0100

    modified response for preflight request

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -15,11 +15,13 @@ class PreflightResponse
      */
     public function handle($request, Closure $next)
     {
-        if($request->getMethod() === 'OPTIONS')
-        {
-
-            return response('');
-
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return \Response::make('OK', 200, $headers);
         }
         
         return $next($request);
