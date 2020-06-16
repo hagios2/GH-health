d84d0828c31b77ddd5dae16f975eadbca7f63a18
hagios <hagioswilson@gmail.com>
2020-06-16 07:13:30 +0100

    modified middleware response

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -15,17 +15,19 @@ class PreflightResponse
      */
     public function handle($request, Closure $next)
     {
-     /*    $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ]; */
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE', 'PATCH',
+            'Access-Control-Allow-Headers' => 'X-PINGOTHER', 'Content-Type, X-Auth-Token, Origin, Authorization'
+       
+        ]; 
+
         if ($request->getMethod() == "OPTIONS") {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return response('', 204)
-            ->header('Access-Control-Allow-Origin', implode(',', config('cors.allow_origins')))
-            ->header('Access-Control-Allow-Methods', implode(',', config('cors.allow_methods')))
-            ->header('Access-Control-Allow-Headers', implode(',', config('cors.allow_headers')))
-            ->header('Access-Control-Max-Age', implode(',', config('cors.max_age')));
+            return response('', 204, $headers);
+         //   ->header('Access-Control-Allow-Origin', implode(',', config('cors.allow_origins')))
+           // ->header('Access-Control-Allow-Methods', implode(',', config('cors.allow_methods')))
+            //->header('Access-Control-Allow-Headers', implode(',', config('cors.allow_headers')))
+           // ->header('Access-Control-Max-Age', implode(',', config('cors.max_age')));
         }
         
         return $next($request);
