5b5870b3dea821c98f1cf8733e5b54395de442b6
hagios <hagioswilson@gmail.com>
2020-09-20 11:49:28 +0100

    added import

--- app/Mail/UserRegistrationMail.php
+++ app/Mail/UserRegistrationMail.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\VerifyEmail;
+use App\User;
 
 class UserRegistrationMail extends Mailable
 {
