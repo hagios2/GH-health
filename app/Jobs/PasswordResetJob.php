ece34909cb9c55eca9c2f3ef151c6a58be6ce050
hagios <hagioswilson@gmail.com>
2020-10-11 20:55:10 +0100

    serveral changes

--- app/Jobs/PasswordResetJob.php
+++ app/Jobs/PasswordResetJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 use App\User;
 use App\ApiPasswordReset;
 use App\Mail\PasswordResetMail;
