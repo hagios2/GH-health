990d0c4013e8cb2b86a4df0f5d643374a22ab08e
hagios <hagioswilson@gmail.com>
2020-10-11 19:31:27 +0100

    serveral changes

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -17,7 +17,7 @@ class PasswordResetController extends Controller
     public function __construct()
     {
       
-        $this->middleware('auth:api,merchandiser', ['only' => ['changeUserPassword', 'changeUserPassword']]);
+        $this->middleware('auth:api,merchandiser', ['only' => ['changeUserPassword', 'changeMediaPassword']]);
         
     }
 
