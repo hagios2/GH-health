c103d2eaba62eff7d453d2154d317eefdc611c24
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-11-29 16:26:25 +0000

    made changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\Resource;
 
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Resource::withoutWrapping();
+
+        Schema::defaultStringLength(191);
     }
 }
