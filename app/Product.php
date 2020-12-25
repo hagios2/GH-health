af7e2c0c6bcd9e2cd89b1f42c6433edd4031cfa8
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-12-25 21:10:17 +0000

    made change

--- app/Product.php
+++ app/Product.php
@@ -18,7 +18,7 @@ class Product extends Model implements Searchable
            $this,
            $this->product_name,
            json_encode(['
-                product_images' => ProductImage::query()->where('product_id', $this->id)->latest()->get()
+                product_images' => ProductImage::where('product_id', $this->id)->latest()->get()
            ]),
            $url
         );
