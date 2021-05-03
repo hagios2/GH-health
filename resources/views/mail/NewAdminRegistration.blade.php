4532e6aba75e30b718324763579db52713b2a87d
Emmanuel Oteng Wilson <ewilson@npontu.com>
2021-05-03 13:01:36 +0000

    made made change

--- resources/views/mail/NewAdminRegistration.blade.php
+++ resources/views/mail/NewAdminRegistration.blade.php
@@ -20,7 +20,7 @@ Thanks,<br>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Verification Email</title>
+    <title>User Verificaion Email</title>
     <style type="text/css">
         body {
             Margin: 0;
