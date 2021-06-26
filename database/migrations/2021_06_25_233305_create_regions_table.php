41663125fff197e99a2d6cdb70dff291f84bda8a
Emmanuel Oteng Wilson <ewilson@npontu.com>
2021-06-26 09:25:35 +0000

    added more validation

--- database/migrations/2021_06_25_233305_create_regions_table.php
+++ database/migrations/2021_06_25_233305_create_regions_table.php
@@ -16,12 +16,12 @@ class CreateRegionsTable extends Migration
         Schema::create('regions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('name_of_regional_minister');
-            $table->string('address_of_regional_minister');
-            $table->string('name_of_director_general');
-            $table->string('address_of_director_general');
-            $table->string('name_of_regional_health_director');
-            $table->string('address_of_regional_health_director');
+            $table->string('name_of_regional_minister')->nullable();
+            $table->string('address_of_regional_minister')->nullable();
+            $table->string('name_of_director_general')->nullable();
+            $table->string('address_of_director_general')->nullable();
+            $table->string('name_of_regional_health_director')->nullable();
+            $table->string('address_of_regional_health_director')->nullable();
             $table->timestamps();
         });
     }
