9c1fa93d9452928a151d8e5d27a2af36e4184e09
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-10-02 14:14:51 +0000

    added admin dashboard

--- app/Http/Controllers/Admin/DashController.php
+++ app/Http/Controllers/Admin/DashController.php
@@ -14,7 +14,7 @@ class DashController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:admin');
+//        $this->middleware('auth:admin');
     }
 
     public function getStats(Request $request): \Illuminate\Http\JsonResponse
