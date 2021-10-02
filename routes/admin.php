a4a187891414f37b024b3a80e1ee87cf9f4c297e
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-10-02 14:23:00 +0000

    added admin dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -95,7 +95,7 @@ Route::delete('delete/{user}/facilitator', 'AdminsController@deleteFacilitator')
 
 #--------------------------- End Facilitator -----------------------------------------------------
 
-Route::get('fetch/dashboard/stats', 'DashboardController@getStats');
+Route::get('fetch/dashboard/stats', 'DashBoardController@getStats');
 
 
 Route::fallback(function(){
