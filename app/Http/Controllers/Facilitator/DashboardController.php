071ad13bebc7e19bf2222a637d7c09a4fa5368a3
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-10-16 05:17:27 +0000

    testing weekly data

--- app/Http/Controllers/Facilitator/DashboardController.php
+++ app/Http/Controllers/Facilitator/DashboardController.php
@@ -101,7 +101,7 @@ class DashboardController extends Controller
     public function fetchVictimStats(Carbon $start_date, Carbon $end_date, Request $request): \Illuminate\Database\Eloquent\Builder
     {
         return Victim::query()
-            ->select(DB::raw('count(id), created_at'))
+            ->select(DB::raw('count(id), CAST(created_at AS DATE)'))
             ->whereBetween('created_at', [$start_date, $end_date]);
     }
 
